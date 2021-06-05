12d799ace4bf10ba8f1470422d409e6c105c859b
Stjepovic <stjepovicmatej2@gmail.com>
2021-06-05 14:40:23 +0200

    InitialCommit
    prvi comit, dobio extension iz upisanog patha.

---
+++ File Reader/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+
+namespace File_Reader
+{
+    class Program
+    {
+        public static string path;
+        public static string extension;
+        static void Main(string[] args)
+        {
+            GetFilePath();
+            ReadFile(path);          
+        }     
+
+        private static void GetFilePath()
+        {
+            Console.WriteLine("Write file path");
+            path = Console.ReadLine();
+            Console.WriteLine(path);           
+            extension = Path.GetExtension(path);
+            Console.WriteLine(extension);
+        }
+        private static void ReadFile(string path)
+        {
+            
+        }
+    }
+}
